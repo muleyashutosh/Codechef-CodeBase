9e1150f1b8e1edadfda6a691563a19430a152691
Ashutosh Muley <muleyashutosh@gmail.com>
2020-06-08 22:54:35 +0530

    Initial Commit

--- Tom_And_Jerry.cpp
+++ Tom_And_Jerry.cpp
@@ -9,19 +9,14 @@ int main () {
         cin >> TS;
         long long JS_count = 0;
         if(TS % 2 != 0) {
-            for(long long i = 2; i <= TS ; i =(long long) i + 2) {
-                JS_count++;
-            }
+                JS_count = (long long) (TS - 1) / 2;
         }
         else {
+        
             while(TS % 2 == 0) {
                 TS /= 2;
             }
-            
-            for(long long i = 2; i <= TS ; i =(long long) i + 2) {
-                JS_count++;
-            }
-            
+                JS_count = (long long) (TS - 1) / 2;
         }
         
         cout << JS_count << endl;
